2fec28cd5b517e105ac92843fbd342d79c578fd3
mcarlg <mcarlg@petricore.dev>
2026-01-19 02:28:56 -0500

    Requirements file added, and basic argparse added.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,20 @@
+import argparse
+
 if __name__ == "__main__":
     main()
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        description="CVE Priority Engine"
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="Verbose logging."
+    )
+
+    args = parser.parse_args()
+
+    if args.verbose:
+        print(f"[*] Verbose logging enabled: {args.verbose} (obviously.)")
